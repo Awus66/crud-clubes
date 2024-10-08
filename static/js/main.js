c8ef99e494479558eea0048b7ffb528d2db5ae1e
Awus66 <conde.agustin66@gmail.com>
2024-10-08 17:47:10 -0300

    Adds map for stadium location

--- static/js/main.js
+++ static/js/main.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+window.addEventListener('load', loadGoogleMapsScript);
+
 
 async function deleteClub(id) {
     const confirmed = confirm('Are you sure you want to delete this club?');
@@ -31,3 +33,40 @@ async function deleteClub(id) {
         alert('An error occurred while deleting the club');
     }
 }
+
+
+function createMap() {
+    const geocoder = new google.maps.Geocoder();
+    const mapElement = document.querySelector('#map');
+    const address = mapElement.getAttribute('data-address');
+
+    geocoder.geocode({ address: address }, function(results, status) {
+      if (status === 'OK') {
+        const map = new google.maps.Map(mapElement, {
+          zoom: 15,
+          center: results[0].geometry.location
+        });
+
+        const marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode could not complete the request for the following reasons: ' + status);
+      }
+    });
+  }
+
+
+function loadGoogleMapsScript() {
+    const mapExists = document.querySelector('#map');
+    if (!mapExists) {
+        return;
+    }
+  
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyB1ts7GIALiMN_BKhkHgZQ8mRvnVrcar6k&callback=createMap`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  }
