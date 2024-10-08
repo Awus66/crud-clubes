c663d4a1ae504a9cb52bf8ab1f3e572a7383f24f
Awus66 <conde.agustin66@gmail.com>
2024-10-08 18:51:20 -0300

    Adds map for club address

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const clubRoutes = require('./controllers/clubsController');
 const PORT = 8080;
+const dotenv = require('dotenv').config();
+
 
 const hbs = exphbs.create({
     defaultLayout: "main", 
@@ -20,6 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());    
 
 app.use('/', clubRoutes);
+
+app.get('/api/google-maps-key', (req, res) => {
+  res.json({ key: process.env.GOOGLE_MAPS_API_KEY });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
